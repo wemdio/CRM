62fdc91c029cb89ac79821ea1a768e0b48e8e14e
Wemdio <vaver1954@mail.ru>
2025-12-14 01:59:58 +0400

    Force display_errors in dev mode

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    // Explicitly enabling debug if APP_ENV is dev
+    if ($context['APP_ENV'] === 'dev') {
+        ini_set('display_errors', '1');
+        error_reporting(E_ALL);
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
