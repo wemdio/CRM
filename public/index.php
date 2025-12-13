9042bfebd44dcd5c33587805898cffdbe5198698
Wemdio <vaver1954@mail.ru>
2025-12-14 02:04:05 +0400

    Add Hello World die-debug

--- public/index.php
+++ public/index.php
@@ -1,11 +1,17 @@
 <?php
 
+// DIE HARD DEBUG
+echo "<h1>PHP IS WORKING</h1>";
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+die();
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    // Explicitly enabling debug if APP_ENV is dev
     if ($context['APP_ENV'] === 'dev') {
         ini_set('display_errors', '1');
         error_reporting(E_ALL);
